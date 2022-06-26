62f60d85b43e802bb0f1173e978176c8f7a31f90
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-25 17:49:10 -0700

    updates

--- main.py
+++ main.py
@@ -5,32 +5,42 @@ def guessgame():
     guess_count = 1
     guess = int(input("What is your guess number between 1 to 100? "))
     number = random.randint(1, 100)
-    while guess != number:
-        guess_count += 1
-        if guess > number:
-            print(f"You guessed a higher number. The number was:{number}")
-            guess1 = input("Want to try to guess again? Enter Yes or no ")
-            while guess1.lower() == "Yes":
-                guess = int(input("What is your new guess number? "))
-                '''if guess == number:
-                    print("Hey!, You guessed the perfect number. The number was: ", number)
-                    print(f"Congrats You got the right answer in {guess_count}")
-                break'''
-            if guess1.lower() == "No":
-                print("You did well!")
-            break
-        elif guess < number:
-            print("You guessed a lower number. The number was: ", number)
-            while input("Want to try to guess again? Enter Yes or no ") == 'Yes':
-                guess = int(input("What is your new guess number? "))
-                '''if guess == number:
-                    print("Hey!, You guessed the perfect number. The number was: ", number)
-                    print(f"Congrats You got the right answer in {guess_count}")
-                break'''
-            while input("Want to try to guess again? Enter Yes or No ") == "No":
-                print("You did well!")
-    print("You got the perfect number", number)
+    if guess > number:
+        print(f"You guessed a higher number.")
+        while guess != number:
+            repeat = input("want to guess again enter yes or no")
+            if repeat == "yes":
+                guess_count += 1
+                guess = int(input("What is your guess number between 1 to 100? "))
+                if guess == number:
+                    print(f"You got it right in {guess_count} counts. The number was {number}")
+                elif guess > number:
+                    print("You guess a higher number")
+                elif guess < number:
+                    print("You guess a lower number")
+            elif repeat == 'no':
+                print(f"You did well. Total count {guess_count}. The number was {number}")
+                break
 
+    elif guess < number:
+        print("You guessed a lower number. The number was: ", number)
+        while guess != number:
+            repeat = input("want to guess again enter yes or no")
+            if repeat == "yes":
+                guess_count += 1
+                guess = int(input("What is your guess number between 1 to 100? "))
+                if guess == number:
+                    print(f"You got it right in {guess_count} counts. The number was {number}")
+                elif guess > number:
+                    print("You guess a higher number")
+                elif guess < number:
+                    print("You guess a lower number")
+            elif repeat == 'no':
+                print(f"You did well. Total count {guess_count}. The number was {number}")
+                break
+
+    elif guess == number:
+        print(f"You got the perfect number in {guess_count}. The number was {number}")
 
 
 guessgame()
